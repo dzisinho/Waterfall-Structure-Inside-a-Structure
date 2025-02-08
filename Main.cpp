952affce451d5228a892ecce196b693ceb004072
dzisinho <dzisbogdan26@gmail.com>
2025-02-08 16:50:34 +0200

    completed

--- Main.cpp
+++ Main.cpp
@@ -1,2 +1,19 @@
 
- 
+#include <iostream>
+using namespace std;
+
+class Car {
+    string brand;
+    int speed;
+public:
+    Car(string b) : Car(b, 0) {}  
+    Car(string b, int s) : brand(b), speed(s) {}
+
+    void show() { cout << brand << " is driving at " << speed << " km/h\n"; }
+};
+
+int main() {
+    Car car1("BMW"), car2("Tesla", 120);
+    car1.show();
+    car2.show();
+}
