214706966889916b035764c00da89a5e90e6e4c0
dzisinho <dzisbogdan26@gmail.com>
2025-02-08 17:15:18 +0200

    completed

--- Main.cpp
+++ Main.cpp
@@ -1,2 +1,25 @@
+#include <iostream>
+using namespace std;
 
- 
+struct Stats {
+    int goals;
+};
+
+struct Player {
+    string name;
+    Stats stats;
+
+    void score() {
+        stats.goals++;
+        cout << name << " scores! Total goals: " << stats.goals << endl;
+    }
+};
+
+int main() {
+    Player player1 = { "Messi", {0} };
+    Player player2 = { "Ronaldo", {0} };
+
+    player1.score();
+    player2.score();
+    player1.score();
+}
